919ae952a1552a92a572718c73d6e87b282f3d39
Patrice-H <webmestre64@gmail.com>
2022-12-02 16:24:16 +0100

    Add data of all projects

---
+++ data/projects.js
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    name: 'Booki',
+    url: 'https://patrice-h.github.io/booki/',
+  },
+  {
+    name: 'Ohmyfood',
+    url: 'https://patrice-h.github.io/PatriceHochard_3_17112021/',
+  },
+  {
+    name: 'GameOn',
+    url: 'https://patrice-h.github.io/PatriceHochard_4_08122021/',
+  },
+  {
+    name: 'Fisheye',
+    url: 'https://patrice-h.github.io/PatriceHochard_6_03012022/',
+  },
+  {
+    name: 'Les petits plats',
+    url: 'https://patrice-h.github.io/PatriceHochard_7_01022022/',
+  },
+  {
+    name: 'Kasa',
+    url: 'https://patrice-h.github.io/PatriceHochard_11_05052022/',
+  },
+  {
+    name: 'SportSee',
+    url: 'https://patrice-h.github.io/PatriceHochard_12_30052022/#/profile/18',
+  },
+];
