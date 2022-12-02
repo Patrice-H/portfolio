44ab98ff1984aa8d54ca07fc07e01c220ce69428
Patrice-H <webmestre64@gmail.com>
2022-12-02 22:16:32 +0100

    Update with abstract and technologies

--- data/projects.js
+++ data/projects.js
@@ -2,29 +2,43 @@ export const projects = [
   {
     name: 'Booki',
     url: 'https://patrice-h.github.io/booki/',
+    abstract: "Site web de réservation d'hébergement",
+    technologies: ['HTML 5', 'CSS 3'],
   },
   {
     name: 'Ohmyfood',
     url: 'https://patrice-h.github.io/PatriceHochard_3_17112021/',
+    abstract: 'Site web permettant de composer son menu',
+    technologies: ['HTML 5', 'SASS'],
   },
   {
     name: 'GameOn',
     url: 'https://patrice-h.github.io/PatriceHochard_4_08122021/',
+    abstract: 'Site web de concours de jeux vidéo',
+    technologies: ['HTML 5', 'CSS 3', 'JavaScript'],
   },
   {
     name: 'Fisheye',
     url: 'https://patrice-h.github.io/PatriceHochard_6_03012022/',
+    abstract: 'Site web de photographes',
+    technologies: ['HTML 5', 'CSS 3', 'JavaScript', 'accessibilité web'],
   },
   {
     name: 'Les petits plats',
     url: 'https://patrice-h.github.io/PatriceHochard_7_01022022/',
+    abstract: 'Site web de recette de cuisine',
+    technologies: ['HTML 5', 'CSS 3', 'JavaScript', 'algorithmie'],
   },
   {
     name: 'Kasa',
     url: 'https://patrice-h.github.io/PatriceHochard_11_05052022/',
+    abstract: 'Application de location de logement',
+    technologies: ['React', 'react router'],
   },
   {
     name: 'SportSee',
     url: 'https://patrice-h.github.io/PatriceHochard_12_30052022/#/profile/18',
+    abstract: 'Application sportive',
+    technologies: ['React', 'react router', 'recharts'],
   },
 ];
