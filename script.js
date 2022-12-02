7216d7a8d3f9a5fb7e9039e633bebd5365eb4f36
Patrice-H <webmestre64@gmail.com>
2022-12-02 11:33:47 +0100

    Display all skills

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { skills } from './data/skills.js';
+
 const toggleShadow = (element) => {
   let scrollBar = window.scrollY;
   if (scrollBar === 0) {
@@ -7,5 +9,21 @@ const toggleShadow = (element) => {
   }
 };
 
+const displaySkills = () => {
+  const skillsContainer = document.getElementById('skills-container');
+  skills.forEach((skill) => {
+    let figure = document.createElement('figure');
+    let img = document.createElement('img');
+    let figcaption = document.createElement('figcaption');
+    img.setAttribute('src', `assets/${skill.file}`);
+    img.setAttribute('alt', skill.name);
+    figcaption.innerHTML = skill.name;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    skillsContainer.appendChild(figure);
+  });
+};
+
 const navbar = document.getElementById('navbar');
 window.addEventListener('scroll', () => toggleShadow(navbar));
+displaySkills();
