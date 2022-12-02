3ab8f75c1cd88358dbd63d46ca69490757345793
Patrice-H <webmestre64@gmail.com>
2022-12-02 11:32:12 +0100

    Add data of skills

---
+++ data/skills.js
@@ -0,0 +1,62 @@
+export const skills = [
+  {
+    name: 'HTML 5',
+    file: 'html5.png',
+  },
+  {
+    name: 'CSS 3',
+    file: 'css3.png',
+  },
+  {
+    name: 'JavaScript',
+    file: 'javascript.png',
+  },
+  {
+    name: 'React',
+    file: 'react.png',
+  },
+  {
+    name: 'Redux',
+    file: 'redux.png',
+  },
+  {
+    name: 'Node.js',
+    file: 'nodejs.png',
+  },
+  {
+    name: 'PHP',
+    file: 'php.png',
+  },
+  {
+    name: 'Java',
+    file: 'java.png',
+  },
+  {
+    name: 'Mongo DB',
+    file: 'mongodb.png',
+  },
+  {
+    name: 'Maria DB',
+    file: 'mariadb.png',
+  },
+  {
+    name: 'MySQL',
+    file: 'mysql.png',
+  },
+  {
+    name: 'Photoshop',
+    file: 'photoshop.png',
+  },
+  {
+    name: 'Illustrator',
+    file: 'illustrator.png',
+  },
+  {
+    name: 'Figma',
+    file: 'figma.png',
+  },
+  {
+    name: 'GitHub',
+    file: 'github.png',
+  },
+];
